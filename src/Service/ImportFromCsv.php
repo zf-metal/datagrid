25ad6892f05ec3cd9f6da7b7d01165a275c6e653
Alejandro Furgeri <alejandro.furgeri@sondeos.com.ar>
2018-05-03 00:22:26 -0300

    update

--- src/Service/ImportFromCsv.php
+++ src/Service/ImportFromCsv.php
@@ -32,7 +32,13 @@ class ImportFromCsv
      *
      * @var array
      */
-    private $columnsName = array();
+    private $columnNames = array();
+
+    /**
+     *
+     * @var array
+     */
+    private $fieldNames = array();
 
     /**
      *
@@ -164,6 +170,8 @@ class ImportFromCsv
 
         if (count($this->columnsConfig) == 0)
             $this->getEntityColumns($this->entity);
+        else
+            $this->getFieldNamesFromConfig();
 
         $this->validateColumnsName();
 
@@ -184,7 +192,7 @@ class ImportFromCsv
         $properties = $this->getEm()->getClassMetadata($entity)->getReflectionProperties();
 
         foreach ($properties as $property) {
-            $this->columnsConfig[] = $property->name;
+            $this->fieldNames[] = $property->name;
         }
     }
 
@@ -193,35 +201,121 @@ class ImportFromCsv
         // En primer lugar supongo que el separador es el punto y coma.
         $this->delimiter = ';';
         // Leo nombre de columnas
-        $this->columnsName = fgetcsv($file, 0, $this->delimiter, "\"", "\"");
+        $this->columnNames = fgetcsv($file, 0, $this->delimiter, "\"", "\"");
 
         // En caso de que la cantidad de nombres sea uno, cambio el separador por coma, y vuelvo a procesar.
-        if (count($this->columnsName) == 1) {
+        if (count($this->columnNames) == 1) {
             $this->delimiter = ',';
-            $this->columnsName = trim(strtoupper(explode(",", $this->columnsName[0])));
+            $this->columnNames = trim(strtoupper(explode(",", $this->columnNames[0])));
         }
     }
 
     private function validateColumnsName()
     {
-        for ($i = 0; $i < count($this->columnsConfig); $i++) {
-            if (array_search($this->columnsConfig[$i], $this->columnsName, false) === false)
-                throw new \Exception("No se encontro la columna " . $this->columnsConfig[$i]);
-        }
+        if (count(array_diff($this->fieldNames, $this->columnNames)) != 0)
+            throw new \Exception("The fields not match");
     }
 
     private function getRows($file)
     {
-        $countNames = count($this->columnsName);
+        $countNames = count($this->fieldNames);
         $this->rows = array();
         while (($data = fgetcsv($file, 0, $this->delimiter, "\"", "\"")) !== FALSE) {
 
             $reg = array();
             for ($i = 0; $i < $countNames; $i++) {
-                $reg[$this->columnsName[$i]] = trim($data[$i]);
+                $columnConfig = $this->getColumnConfigFromValue($this->fieldNames[$i]);
+                $index = $columnConfig ? key($columnConfig) : $this->fieldNames[$i];
+                $reg[$index] = $this->buildValue($columnConfig[$index], $data[$i]);
             }
             $this->rows[] = $reg;
         }
+    }
+
+    private function getColumnConfigFromValue($value)
+    {
+        foreach ($this->columnsConfig as $index => $config) {
+
+            if ((isset($config['displayName']) && $config['displayName'] == $value) || $index == $value)
+                return array($index => $config);
+        }
+
+        return null;
+    }
+
+    private function buildValue($config = array(), $value = null)
+    {
+        if (empty($config))
+            return $value ? (string)$value : null;
+
+        $type = isset($config['type']) ? $config['type'] : 'string';
+
+        $result = null;
+
+        switch ($type) {
+            case 'relational':
+                $result = $this->getRelationalValue($config, $value);
+                break;
+            case 'datetime':
+                $result = $this->getDatetimeValue($config, $value);
+                break;
+            case 'boolean':
+                $result = $this->getBooleanValue($config, $value);
+                break;
+            default:
+                $result = (string)$value || null;
+        }
+
+        return $result;
+    }
+
+    private function getRelationalValue($config = array(), $value = null)
+    {
+        if (!$value && !isset($config['default']))
+            return null;
+
+        if (!isset($config['field']) || !isset($config['entity']))
+            throw new \Exception('failed config on getRelationalValue');
+
+        $obj = null;
+
+        $value = $value ? $value : $config['default'];
+
+        if ($config['field'] == 'id')
+            $obj = $this->getEm()->getReference($config['entity'], $value);
+        else
+            $obj = $this->getEm()->getRepository($config['entity'])->findOneBy([$config['field'], $value]);
+
+        return $obj;
+    }
+
+    private function getDatetimeValue($config = array(), $value = null)
+    {
+        $format = isset($config['format']) && !empty($config['format']) ? $config['format'] : 'Y-m-d H:i:s';
+
+        if ($value)
+            return \DateTime::createFromFormat($format, $value);
+
+        $default = isset($config['default']) && !empty($config['default']) ? $config['default'] : null;
+
+        switch ($default) {
+            case 'now':
+                return new \DateTime();
+            case 'curdate':
+                return new \DateTime('Y-m-d');
+        }
+
+        return null;
+    }
+
+    private function getBooleanValue($config = array(), $value = null)
+    {
+        if (isset($config['valueOfTrue']))
+            return $config['valueOfTrue'] == $value ? true : false;
+        elseif (isset($config['valueOfFalse']))
+            return $config['valueOfFalse'] == $value ? false : true;
+
+        return $value ? true : false;
     }
 
     private function persistRegisters()
@@ -261,20 +355,32 @@ class ImportFromCsv
                 throw new \Exception('La key ' . $configKey . ' no existe en zf-metal-commons.imports!');
             }
             $this->customConfig = $this->getConfig()[$configKey];
+
             if (isset($this->customConfig['columnsConfig']))
                 $this->columnsConfig = $this->customConfig['columnsConfig'];
         }
 
-        if(empty($this->columnsConfig))
+        if (empty($this->columnsConfig))
             $this->getEntityColumns($this->entity);
+        else
+            $this->getFieldNamesFromConfig();
 
         $this->createFileExample();
+    }
+
+    private function getFieldNamesFromConfig()
+    {
+        $this->fieldNames = array();
+
+        foreach ($this->columnsConfig as $index => $values) {
+            $this->fieldNames[] = isset($values['displayName']) && !empty($values['displayName']) ? $values['displayName'] : $index;
+        }
     }
 
     private function createFileExample()
     {
         $csv = new CSVWriter();
-        $csv->addRow($this->columnsConfig);
+        $csv->addRow($this->fieldNames);
         $csv->setDelimiter($this->delimiter ? $this->delimiter : '; ');
         $csv->saveFile($this->entity . '-Example');
     }
